5bca33cbf3919c565b319646e724455657e95972
Adrian Luis Mamani Quispe <adrianluis948826@gmail.com>
2026-09-20 08:13:03 -0500

    orden1, o-1.1

--- ordenación1.cpp
+++ ordenación1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void ordenar(int v[], int n)
+void IDD(int v[], int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
@@ -9,11 +9,31 @@ void ordenar(int v[], int n)
         {
             if (v[j] > v[j + 1])
             {
-                // Intercambiar v[j] y v[j+1]
-                int temp = v[j];
+                int aux = v[j];
                 v[j] = v[j + 1];
-                v[j + 1] = temp;
+                v[j + 1] = aux;
             }
         }
     }
+}
+
+int main()
+{
+    int n;
+    cout << "Ingrese el tamaño del arreglo: ";
+    cin >> n;
+    int v[n];
+    cout << "Ingrese los elementos del arreglo: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    IDD(v, n);
+    cout << "Arreglo ordenado: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
